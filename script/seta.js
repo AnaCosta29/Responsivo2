a9f87c42c4f242221da5fe2c3d9ed09821c3d350
Ana Costa <luizacosta64@hotmail.com>
2019-04-11 22:19:34 -0300

    incrementojs

--- script/seta.js
+++ script/seta.js
@@ -5,8 +5,6 @@ for (i = 0; i < x.length; i++) {
   x[i].addEventListener("click", function() {
     
     this.classList.toggle("active");
-
-   
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
